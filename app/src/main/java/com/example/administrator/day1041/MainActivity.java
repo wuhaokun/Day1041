7b2e49946c93e99919855d099178816f8e3358c0
wuhaokun <1210107052@163.com>
2016-10-04 18:59:08 +0800

    2222

---
+++ app/src/main/java/com/example/administrator/day1041/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.administrator.day1041;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+    }
+
+    int x =4;
+
+    private void initData() {
+    }
+
+    private void initView() {
+
+    }
+}
